70fbcc81897a7772b2eb0faf1307115d751b98d5
michelle <michelle.graham@dkit.ie>
2026-02-12 15:00:52 +0000

    test(DynamicArray) : add hard-coded testing confirming remove at position works

--- src/main/java/apps/DynamicArrayTestBed.java
+++ src/main/java/apps/DynamicArrayTestBed.java
@@ -6,6 +6,18 @@ import utils.DynamicArray;
 public class DynamicArrayTestBed {
     public static void main() {
         DynamicArray myList = new DynamicArray();
+
+        for (int i = 0; i < 10; i++) {
+            myList.add("String " + i);
+        }
+
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.println(i + ") " + myList.get(i));
+        }
+
+        String removed = myList.remove(8);
+        System.out.println(removed + " removed from position 8");
+
         for (int i = 0; i < myList.getSize(); i++) {
             System.out.println(i + ") " + myList.get(i));
         }
