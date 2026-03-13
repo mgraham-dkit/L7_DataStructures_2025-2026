30adfd332f5e5f6c3bc509071627e74b9b121f73
michelle <michelle.graham@dkit.ie>
2026-03-13 15:22:09 +0000

    test(LinkedList) : add manual testing for removing from list at specified position

--- src/main/java/apps/LinkedListTestBed.java
+++ src/main/java/apps/LinkedListTestBed.java
@@ -11,9 +11,8 @@ public class LinkedListTestBed {
             myList.add("String " + i);
         }
 
-        for (int i = 0; i < myList.getSize(); i++) {
-            System.out.println(i + ") " + myList.get(i));
-        }
+        displayList(myList);
+
         long start = System.nanoTime();
         int index = myList.lastIndexOf("String 9");
         long end = System.nanoTime();
@@ -21,5 +20,34 @@ public class LinkedListTestBed {
 
         System.out.println("String 9 was found at position " + index);
         System.out.println("Time taken to find element: " + timeTaken);
+
+        // Test remove logic - remove from start
+        int removeIndex = 0;
+        String removed = myList.remove(removeIndex);
+        System.out.println("Removed from " + removeIndex + ": " + removed);
+        displayList(myList);
+
+        // Remove from end
+        removeIndex = myList.getSize()-1;
+        removed = myList.remove(removeIndex);
+        System.out.println("Removed from " + removeIndex + ": " + removed);
+        displayList(myList);
+
+        // Confirm last was updated correctly
+        System.out.println("Adding new last element (String 10)");
+        myList.add("String 10");
+        displayList(myList);
+
+        // Remove from middle
+        removeIndex = myList.getSize()/2;
+        removed = myList.remove(removeIndex);
+        System.out.println("Removed from " + removeIndex + ": " + removed);
+        displayList(myList);
+    }
+
+    private static void displayList(LinkedList myList) {
+        for (int i = 0; i < myList.getSize(); i++) {
+            System.out.println(i + ") " + myList.get(i));
+        }
     }
 }
