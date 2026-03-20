be6db041c11e152bdfeee65c3eaf34ef8c8637f6
michelle <michelle.graham@dkit.ie>
2026-03-20 13:50:35 +0000

    feat(ListSet): add basic ListSet composed of LinkedList

---
+++ src/main/java/utils/ListSet.java
@@ -0,0 +1,41 @@
+package utils;
+
+public class ListSet {
+    private LinkedList list;
+
+    public ListSet(){
+        this.list = new LinkedList();
+    }
+
+    public int getSize(){
+        return list.getSize();
+    }
+
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
+
+    public int indexOf(String element){
+        return list.indexOf(element);
+    }
+
+    public boolean contains(String element){
+        return list.contains(element);
+    }
+
+    public String get(int index){
+        return list.get(index);
+    }
+
+    public void clear(){
+        list.clear();
+    }
+
+    public String remove(int index){
+        return list.remove(index);
+    }
+
+    public boolean remove(String element){
+        return list.remove(element);
+    }
+}
